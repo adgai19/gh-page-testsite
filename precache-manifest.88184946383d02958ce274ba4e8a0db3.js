32e540e5020cbc266df9e3968fb50ea84c04a84d
adgai19 <gaitondeaditya@gmail.com>
2020-07-22 19:47:58 +0530

    Updates

---
+++ precache-manifest.88184946383d02958ce274ba4e8a0db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8172f20d2031a66b763e81fab9e385c7",
+    "url": "/gh-page-testsite/index.html"
+  },
+  {
+    "revision": "0fc885d36eba83d107a4",
+    "url": "/gh-page-testsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98070a80a64b4b235e80",
+    "url": "/gh-page-testsite/static/js/2.e855ec27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-page-testsite/static/js/2.e855ec27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc885d36eba83d107a4",
+    "url": "/gh-page-testsite/static/js/main.6bca9cbb.chunk.js"
+  },
+  {
+    "revision": "88a9d2afc18c671ad8fc",
+    "url": "/gh-page-testsite/static/js/runtime-main.232ee338.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-page-testsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
